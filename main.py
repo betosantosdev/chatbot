ae4a894194e9d02518114c9dd8f8505a97ea68da
Beto Santos <pauloroberto520@gmail.com>
2022-12-02 08:31:08 -0300

    Uploaded files

---
+++ main.py
@@ -0,0 +1,141 @@
+import nltk
+#nltk.download()
+from nltk.stem import *
+from nltk.stem.lancaster import LancasterStemmer
+
+from keras.models import model_from_json
+import os
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+with open("intents.json") as file:
+	data = json.load(file)
+
+try:
+	with open("data.picke", "rb") as f:
+		words, labels, training, output = picke.load(f)
+
+except:
+	words = []
+	labels = []
+	docs_x = []
+	docs_y = []
+
+	for intent in data['intents']:
+		for pattern in intent["patterns"]:
+			wrds = nltk.word_tokenize(pattern)
+			words.extend(wrds)
+			docs_x.append(wrds)
+			docs_y.append(intent["tag"])
+
+		if intent["tag"] not in labels:
+			labels.append(intent["tag"])
+
+	words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+	words = sorted(list(set(words)))
+
+	labels = sorted(labels)
+
+	training = []
+	output = []
+
+	out_empty = [0 for _ in range(len(labels))]
+
+	for x, doc in enumerate(docs_x):
+		bag = []
+
+		wrds = [stemmer.stem(w) for w in doc]
+
+		for w in words:
+			if w in wrds:
+				bag.append(1)
+			else:
+				bag.append(0)
+
+		output_row = out_empty[:]
+		output_row[labels.index(docs_y[x])] = 1
+
+		training.append(bag)
+		output.append(output_row)
+
+	training = numpy.array(training)
+	output = numpy.array(output)
+
+	with open("data.picke", "wb") as f:
+		pickle.dump((words, labels, training, output), f)
+
+
+tensorflow.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+
+
+
+
+def bag_of_words(s, words):
+	bag = [0 for _ in range(len(words))]
+
+	s_words = nltk.word_tokenize(s)
+	s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+	for se in s_words:
+		for i, w in enumerate(words):
+			if w == se:
+				bag[i] = 1
+
+	return numpy.array(bag)
+
+
+def chat():
+	print("Start talking with the bot!")
+	while True:
+		inp = input("You: ")
+		if inp.lower() == "quit":
+			break
+
+		results = model.predict([bag_of_words(inp,words)])[0]
+		results_index = numpy.argmax(results)
+		tag = labels[results_index]
+		
+		if results[results_index] > 0.7:
+			for tg in data["intents"]:
+				if tg['tag'] == tag:
+					responses = tg['responses']
+
+			print(random.choice(responses))
+
+		else:
+			print("I don't know, what should I say?")
+			model.fit(training, output, n_epoch=100, batch_size=8, show_metric=True)
+			
+
+
+def save_model(model, name):
+    json_string = model.to_json()
+    if not os.path.isdir('cache'):
+        os.mkdir('cache')
+    open(os.path.join('cache', '%s.json' % name), 'w').write(json_string)
+    model.save_weights(os.path.join('cache', '%s_weights.h5' % name), overwrite=True)
+
+
+def read_model(name):
+    model = model_from_json(open(os.path.join('cache', '%s.json' % name)).read())
+    model.load_weights(os.path.join('cache', '%s_weights.h5' % name))
+    return model
+
+chat()
